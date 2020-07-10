5b6b698441a929fb60da90b9ce54846c78477405
Joel Schecheleski <joel.schecheleski@envolti.com>
2020-07-10 14:13:27 -0300

    Adicionado biblioteca fake do bge

--- Player.py
+++ Player.py
@@ -1,6 +1,6 @@
 from bge import logic, events
 
-keys = logic.keyboard.inputs
+tc = logic.keyboard.inputs
 mouseKeys = logic.mouse.inputs
 
 keyTAP = logic.KX_INPUT_JUST_ACTIVATED
@@ -19,5 +19,11 @@ def update(controller):
 
     owner["inventário"].append(1)
 
-    if keys[events.WKEY].values[-1]:
+    if tc[events.WKEY].values[-1]:
         owner.applyMovement([0, 0.1, 0], True)
+    elif tc[events.SKEY].values[-1]:
+        owner.applyMovement([0, -0.1, 0], True)
+    elif tc[events.AKEY].values[-1]:
+        owner.applyRotation([0, 0, 0.05], True)
+    elif tc[events.DKEY].values[-1]:
+        owner.applyRotation([0, 0, -0.05], True)
